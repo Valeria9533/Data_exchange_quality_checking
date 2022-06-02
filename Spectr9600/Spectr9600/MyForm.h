2ebf4b9524b733a54d6cf07b366590d1f8ededa2
Valeriia Okromelidze <79271270+Valeria9533@users.noreply.github.com>
2022-06-02 11:51:30 +0300

    Add files via upload

--- Spectr9600/Spectr9600/MyForm.h
+++ Spectr9600/Spectr9600/MyForm.h
@@ -81,7 +81,6 @@ namespace Spectr9600 {
 		}
 
 	private: System::Windows::Forms::CheckBox^  checkBox1;
-
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::TextBox^  textBox1;
@@ -94,7 +93,6 @@ namespace Spectr9600 {
 	private: System::Windows::Forms::Label^  label3;
 	private: System::Windows::Forms::Label^  label4;
 	private: System::Windows::Forms::GroupBox^  groupBox1;
-
 	private: System::ComponentModel::BackgroundWorker^  backgroundWorker1;
 	private: System::ComponentModel::BackgroundWorker^  backgroundWorker2;
 	private: System::Windows::Forms::Label^  label5;
@@ -115,9 +113,11 @@ namespace Spectr9600 {
 	private: System::Windows::Forms::Label^  label10;
 	private: System::Windows::Forms::TextBox^  textBox8;
 	public: System::Windows::Forms::Timer^  timer1;
-
-	public:
-
+	private: System::Windows::Forms::Label^  label13;
+	private: System::Windows::Forms::Label^  label12;
+	private: System::Windows::Forms::TextBox^  textBox9;
+	private: System::Windows::Forms::Label^  label14;
+	private: System::Windows::Forms::RichTextBox^  richTextBox2;
 	private: System::ComponentModel::IContainer^  components;
 	private:
 		/// <summary>
@@ -157,6 +157,9 @@ namespace Spectr9600 {
 			this->button7 = (gcnew System::Windows::Forms::Button());
 			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->label13 = (gcnew System::Windows::Forms::Label());
+			this->label12 = (gcnew System::Windows::Forms::Label());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
 			this->label11 = (gcnew System::Windows::Forms::Label());
 			this->label10 = (gcnew System::Windows::Forms::Label());
 			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
@@ -167,6 +170,8 @@ namespace Spectr9600 {
 			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
 			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label14 = (gcnew System::Windows::Forms::Label());
 			this->groupBox1->SuspendLayout();
 			this->groupBox3->SuspendLayout();
 			this->SuspendLayout();
@@ -175,7 +180,7 @@ namespace Spectr9600 {
 			// 
 			this->checkBox1->AutoSize = true;
 			this->checkBox1->Enabled = false;
-			this->checkBox1->Location = System::Drawing::Point(365, 77);
+			this->checkBox1->Location = System::Drawing::Point(409, 77);
 			this->checkBox1->Name = L"checkBox1";
 			this->checkBox1->Size = System::Drawing::Size(58, 17);
 			this->checkBox1->TabIndex = 0;
@@ -241,20 +246,20 @@ namespace Spectr9600 {
 			this->richTextBox1->Location = System::Drawing::Point(57, 44);
 			this->richTextBox1->Name = L"richTextBox1";
 			this->richTextBox1->ReadOnly = true;
-			this->richTextBox1->Size = System::Drawing::Size(271, 208);
+			this->richTextBox1->Size = System::Drawing::Size(137, 208);
 			this->richTextBox1->TabIndex = 7;
 			this->richTextBox1->Text = L"";
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(20, 36);
+			this->textBox2->Location = System::Drawing::Point(68, 36);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 9;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(190, 36);
+			this->textBox3->Location = System::Drawing::Point(238, 36);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(100, 20);
 			this->textBox3->TabIndex = 10;
@@ -262,7 +267,7 @@ namespace Spectr9600 {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(17, 20);
+			this->label3->Location = System::Drawing::Point(65, 20);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(45, 13);
 			this->label3->TabIndex = 11;
@@ -271,7 +276,7 @@ namespace Spectr9600 {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(187, 20);
+			this->label4->Location = System::Drawing::Point(235, 20);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(43, 13);
 			this->label4->TabIndex = 12;
@@ -279,6 +284,8 @@ namespace Spectr9600 {
 			// 
 			// groupBox1
 			// 
+			this->groupBox1->Controls->Add(this->label14);
+			this->groupBox1->Controls->Add(this->richTextBox2);
 			this->groupBox1->Controls->Add(this->richTextBox1);
 			this->groupBox1->Controls->Add(this->button2);
 			this->groupBox1->Controls->Add(this->button3);
@@ -287,7 +294,7 @@ namespace Spectr9600 {
 			this->groupBox1->Controls->Add(this->label2);
 			this->groupBox1->Location = System::Drawing::Point(12, 12);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(396, 358);
+			this->groupBox1->Size = System::Drawing::Size(413, 358);
 			this->groupBox1->TabIndex = 17;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Connection testing";
@@ -305,7 +312,7 @@ namespace Spectr9600 {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(17, 177);
+			this->label5->Location = System::Drawing::Point(65, 177);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(39, 13);
 			this->label5->TabIndex = 13;
@@ -314,7 +321,7 @@ namespace Spectr9600 {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(187, 239);
+			this->label6->Location = System::Drawing::Point(235, 239);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(34, 13);
 			this->label6->TabIndex = 14;
@@ -322,7 +329,7 @@ namespace Spectr9600 {
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(20, 193);
+			this->textBox4->Location = System::Drawing::Point(68, 193);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->ReadOnly = true;
 			this->textBox4->Size = System::Drawing::Size(100, 20);
@@ -330,7 +337,7 @@ namespace Spectr9600 {
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(190, 255);
+			this->textBox5->Location = System::Drawing::Point(238, 255);
 			this->textBox5->Name = L"textBox5";
 			this->textBox5->ReadOnly = true;
 			this->textBox5->Size = System::Drawing::Size(100, 20);
@@ -338,7 +345,7 @@ namespace Spectr9600 {
 			// 
 			// textBox6
 			// 
-			this->textBox6->Location = System::Drawing::Point(20, 255);
+			this->textBox6->Location = System::Drawing::Point(68, 255);
 			this->textBox6->Name = L"textBox6";
 			this->textBox6->ReadOnly = true;
 			this->textBox6->Size = System::Drawing::Size(100, 20);
@@ -346,7 +353,7 @@ namespace Spectr9600 {
 			// 
 			// button7
 			// 
-			this->button7->Location = System::Drawing::Point(190, 315);
+			this->button7->Location = System::Drawing::Point(238, 315);
 			this->button7->Name = L"button7";
 			this->button7->Size = System::Drawing::Size(75, 27);
 			this->button7->TabIndex = 23;
@@ -357,7 +364,7 @@ namespace Spectr9600 {
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(17, 239);
+			this->label7->Location = System::Drawing::Point(65, 239);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(60, 13);
 			this->label7->TabIndex = 24;
@@ -365,6 +372,9 @@ namespace Spectr9600 {
 			// 
 			// groupBox3
 			// 
+			this->groupBox3->Controls->Add(this->label13);
+			this->groupBox3->Controls->Add(this->label12);
+			this->groupBox3->Controls->Add(this->textBox9);
 			this->groupBox3->Controls->Add(this->label11);
 			this->groupBox3->Controls->Add(this->label10);
 			this->groupBox3->Controls->Add(this->textBox8);
@@ -394,12 +404,39 @@ namespace Spectr9600 {
 			this->groupBox3->TabIndex = 30;
 			this->groupBox3->TabStop = false;
 			// 
+			// label13
+			// 
+			this->label13->AutoSize = true;
+			this->label13->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.3F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label13->Location = System::Drawing::Point(481, 193);
+			this->label13->Name = L"label13";
+			this->label13->Size = System::Drawing::Size(26, 17);
+			this->label13->TabIndex = 41;
+			this->label13->Text = L"ms";
+			// 
+			// label12
+			// 
+			this->label12->AutoSize = true;
+			this->label12->Location = System::Drawing::Point(406, 177);
+			this->label12->Name = L"label12";
+			this->label12->Size = System::Drawing::Size(105, 13);
+			this->label12->TabIndex = 40;
+			this->label12->Text = L"Transmission interval";
+			// 
+			// textBox9
+			// 
+			this->textBox9->Location = System::Drawing::Point(409, 193);
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(66, 20);
+			this->textBox9->TabIndex = 39;
+			// 
 			// label11
 			// 
 			this->label11->AutoSize = true;
 			this->label11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.3F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label11->Location = System::Drawing::Point(411, 193);
+			this->label11->Location = System::Drawing::Point(455, 255);
 			this->label11->Name = L"label11";
 			this->label11->Size = System::Drawing::Size(20, 17);
 			this->label11->TabIndex = 38;
@@ -408,7 +445,7 @@ namespace Spectr9600 {
 			// label10
 			// 
 			this->label10->AutoSize = true;
-			this->label10->Location = System::Drawing::Point(362, 177);
+			this->label10->Location = System::Drawing::Point(406, 239);
 			this->label10->Name = L"label10";
 			this->label10->Size = System::Drawing::Size(101, 13);
 			this->label10->TabIndex = 37;
@@ -416,7 +453,7 @@ namespace Spectr9600 {
 			// 
 			// textBox8
 			// 
-			this->textBox8->Location = System::Drawing::Point(365, 193);
+			this->textBox8->Location = System::Drawing::Point(409, 255);
 			this->textBox8->Name = L"textBox8";
 			this->textBox8->ReadOnly = true;
 			this->textBox8->Size = System::Drawing::Size(41, 20);
@@ -426,7 +463,7 @@ namespace Spectr9600 {
 			// 
 			this->checkBox3->AutoSize = true;
 			this->checkBox3->Enabled = false;
-			this->checkBox3->Location = System::Drawing::Point(365, 119);
+			this->checkBox3->Location = System::Drawing::Point(409, 119);
 			this->checkBox3->Name = L"checkBox3";
 			this->checkBox3->Size = System::Drawing::Size(53, 17);
 			this->checkBox3->TabIndex = 35;
@@ -437,7 +474,7 @@ namespace Spectr9600 {
 			// label9
 			// 
 			this->label9->AutoSize = true;
-			this->label9->Location = System::Drawing::Point(17, 81);
+			this->label9->Location = System::Drawing::Point(65, 81);
 			this->label9->Name = L"label9";
 			this->label9->Size = System::Drawing::Size(52, 13);
 			this->label9->TabIndex = 34;
@@ -446,7 +483,7 @@ namespace Spectr9600 {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(20, 98);
+			this->comboBox1->Location = System::Drawing::Point(68, 98);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(121, 21);
 			this->comboBox1->TabIndex = 33;
@@ -454,7 +491,7 @@ namespace Spectr9600 {
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(187, 177);
+			this->label8->Location = System::Drawing::Point(235, 177);
 			this->label8->Name = L"label8";
 			this->label8->Size = System::Drawing::Size(43, 13);
 			this->label8->TabIndex = 32;
@@ -462,7 +499,7 @@ namespace Spectr9600 {
 			// 
 			// textBox7
 			// 
-			this->textBox7->Location = System::Drawing::Point(190, 193);
+			this->textBox7->Location = System::Drawing::Point(238, 193);
 			this->textBox7->Name = L"textBox7";
 			this->textBox7->ReadOnly = true;
 			this->textBox7->Size = System::Drawing::Size(100, 20);
@@ -471,7 +508,7 @@ namespace Spectr9600 {
 			// checkBox2
 			// 
 			this->checkBox2->AutoSize = true;
-			this->checkBox2->Location = System::Drawing::Point(365, 36);
+			this->checkBox2->Location = System::Drawing::Point(409, 36);
 			this->checkBox2->Name = L"checkBox2";
 			this->checkBox2->Size = System::Drawing::Size(66, 17);
 			this->checkBox2->TabIndex = 30;
@@ -483,6 +520,24 @@ namespace Spectr9600 {
 			// 
 			this->timer1->Interval = 1000;
 			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			// 
+			// richTextBox2
+			// 
+			this->richTextBox2->Location = System::Drawing::Point(226, 44);
+			this->richTextBox2->Name = L"richTextBox2";
+			this->richTextBox2->ReadOnly = true;
+			this->richTextBox2->Size = System::Drawing::Size(137, 208);
+			this->richTextBox2->TabIndex = 8;
+			this->richTextBox2->Text = L"";
+			// 
+			// label14
+			// 
+			this->label14->AutoSize = true;
+			this->label14->Location = System::Drawing::Point(223, 28);
+			this->label14->Name = L"label14";
+			this->label14->Size = System::Drawing::Size(86, 13);
+			this->label14->TabIndex = 9;
+			this->label14->Text = L"Transmitted data";
 			// 
 			// MyForm
 			// 
@@ -569,6 +624,20 @@ namespace Spectr9600 {
 	private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
 
 		if (checkBox1->Checked == true) {
+
+			try {
+				int time_interval = Convert::ToInt16(textBox9->Text);
+
+				//if (time_interval >= 5) {
+					this->timer1->Interval = time_interval;
+				//}
+				//else {
+				//	MessageBox::Show("Time interval must be >= 5 ms"); checkBox1->Checked = false; return;
+				//}
+			}
+			catch(...) {
+				MessageBox::Show("Enter time interval"); checkBox1->Checked = false; return;
+			}
 
 			button2->Enabled = true;
 			checkBox3->Checked = false;
@@ -672,10 +741,13 @@ namespace Spectr9600 {
 	}
 
 	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
-
+	
 		timer = 1;
 		DispValues();
 		DispRxData();
+		if (checkBox1->Checked == true) {
+			DispTxData();
+		}
 	}
 
 	/* DATA TRANSMISSION */
@@ -716,65 +788,6 @@ namespace Spectr9600 {
 				number++;
 				timer = 0;
 				wait_answer = 1;
-			}
-
-			else if (((CS_WR + errors) > CS_OK) && ((CS_WR + errors) != 0)) {
-				quality = CS_OK / (CS_WR + errors) * 100;
-			}
-
-			/* Get data back */
-			if (serialPort1->IsOpen == true) {
-
-				if ((serialPort1->BytesToRead > 0) && (wait_answer)) {
-
-					Array[i1] = Pack_Rx[0] = serialPort1->BaseStream->ReadByte();
-					i1++;
-
-					switch (ModeRx)
-					{
-						/* find addr */
-						case 0:
-							if (Pack_Rx[0] == my_addr) { ModeRx = 1; CS_Rx = Pack_Rx[0]; }
-							break;
-	
-						/* get num of bytes */
-						case 1:
-							bytes_get_num = Pack_Rx[0];
-							CS_Rx += Pack_Rx[0];
-							ModeRx = 2;
-							count_bytes = 0;
-							break;
-
-						/* get bytes */
-						case 2:
-							if (count_bytes < bytes_get_num) {
-								Pack_Rx[count_bytes + 1] = Pack_Rx[0]; CS_Rx += Pack_Rx[0]; count_bytes++;
-							}
-							else
-							/* get CS */
-							{
-								Pack_Rx[count_bytes + 1] = Pack_Rx[0];
-								if (Pack_Rx[count_bytes + 1] == CS_Rx)
-								{
-									CS_OK++;
-									wait_answer = 0;
-								}
-
-								else  CS_WR++;
-								ModeRx = 0;
-							}
-							break;
-
-						default:
-						ModeRx = 0;
-						break;
-					}
-				}
-			}
-
-			// %
-			if ((CS_WR + errors) <= CS_OK) {
-				quality = (1 - (CS_WR + errors) / CS_OK) * 100;
 			}
 		}
 
@@ -834,11 +847,7 @@ namespace Spectr9600 {
 		}
 
 		// %
-		if ((CS_WR + errors) <= CS_OK) {
-			quality = (1 - (CS_WR + errors) / CS_OK) * 100;
-		}
-
-
+		quality = (CS_OK / num_packs) * 100;
 	}
 
 	private: System::Void backgroundWorker2_DoWork(System::Object^  sender, System::ComponentModel::DoWorkEventArgs^  e) {
@@ -870,9 +879,14 @@ namespace Spectr9600 {
 
 						case 1:
 							bytes_get_num = Pack_Rx[0];
-							CS_Rx += Pack_Rx[0];
-							ModeRx = 2;
-							count_bytes = 0;
+							if (bytes_get_num != 2) {
+								ModeRx = 0;
+							}
+							else {
+								CS_Rx += Pack_Rx[0];
+								ModeRx = 2;
+								count_bytes = 0;
+							}
 							break;
 
 						case 2:
@@ -883,12 +897,14 @@ namespace Spectr9600 {
 								if (Pack_Rx[count_bytes + 1] == CS_Rx)
 								{
 									CS_OK++;
-									x2 = x1;
+				
 									x1 = Pack_Rx[1] + (Pack_Rx[2] << 8);
 
 									if ((x1 > 0) && (x2 > 0) && (x1 > x2)) {
 										errors += (x1 - x2 - 1); 
 									}
+
+									x2 = x1;
 
 									Pack_Rx[0] = tx_addr; 
 									Pack_Rx[1] = 2; 
@@ -928,13 +944,7 @@ namespace Spectr9600 {
 						}
 
 						// %
-						if ((CS_WR + errors) <= CS_OK) {
-							quality = (1 - (CS_WR + errors) / CS_OK) * 100;
-						}
-
-						else if (((CS_WR + errors) > CS_OK) && ((CS_WR + errors) != 0)) {
-							quality = CS_OK / (CS_WR + errors) * 100;
-						}
+						quality = CS_OK / (CS_OK + CS_WR + errors) * 100;
 					}
 				}
 			}
@@ -994,6 +1004,28 @@ namespace Spectr9600 {
 		else {}
 	}
 
+	public: void DispTxData() {
+
+		Byte value;
+
+		if (serialPort1->IsOpen == true) {
+
+			try {
+				for (int i = 0; i < 5; i++) {
+
+					value = Pack_Tx[i]; 				
+					String^ hex_val = String::Format("{0:X}", value); // convert value to a hexadecimal in string form
+
+					richTextBox2->AppendText(Convert::ToString(hex_val));
+					richTextBox2->AppendText("\n");
+					richTextBox2->ScrollToCaret();
+				}
+			}
+			catch (...) {}
+		}
+		else {}
+	}
+
 	/* Transmit data */
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
 
@@ -1024,6 +1056,7 @@ namespace Spectr9600 {
 		textBox7->Clear();
 		textBox8->Clear();
 		richTextBox1->Clear();
+		richTextBox2->Clear();
 		num_packs = 0;
 		errors = 0;
 		CS_OK = 0;
